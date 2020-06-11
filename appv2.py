9584e8a089d88db73f3eafdae36567befa8c104e
Camix <45560235+cameliasamoilescu@users.noreply.github.com>
2020-06-11 22:34:55 +0300

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -54,6 +54,9 @@ class User():
             self.calendar[date].append(event_name)
         else:
             self.calendar[date] = [event_name]
+            
+    def set_name(self, name):
+        self.name = name
 
 
 def get_addresses():
@@ -68,10 +71,35 @@ def get_user(id):
         if user.id== id:
             return user
         
+
+        
+def get_user_by_name(name):
+     for user in users:
+        if lower(user.name) == name:
+            return user
+        return -1
+        
+        
+        
 def add_even(event_name, date, user):
     user.add_event(event_name, date)
     
+    
+
 class Bot(Client):
+
+    
+    
+    def change_nickname(self, author_id, target_user, nickname):
+        target_user.set_name = nickname
+        
+        if author_id == target_user.id:
+            message = text = f"Ti-ai schimbat numele in {nickname}"
+        else:
+            message = text = f"{get_user(author_id).name } ti-a schimbat numele in {nickname}"
+    
+        self.send(Message(text = message), thread_id = target_user.id, thread_type = ThreadType.USER)
+        return message
 
 
     def send_all(self, message, author_id = ""):
@@ -84,14 +112,16 @@ class Bot(Client):
         response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + oras + "&appid=d00e3b645e7b14cceb37659193a27d72")
         data = json.loads(response.text)
         temp = data['main']['temp']
-        temp = temp - 273.15
+
+        temp = round(temp - 273.15, 2)
         for address in get_addresses():
-            self.send(Message(text = 'Sunt ' + str(temp) + ' grade la ' + oras), thread_id = address, thread_type = ThreadType.USER)
+            self.send(Message(text = f"Sunt {str(temp)} grade la {oras}"), thread_id = address, thread_type = ThreadType.USER)
     
             
     def cere_calendar(self, author_id, thread_type):
         url = 'http://107.23.107.21:443/Calendar?name=' + get_user(author_id).name + '&dict=' + str(get_user(author_id).calendar).replace('\'', "\"").replace(' ','') 
         self.send(Message(text = url), thread_id = author_id, thread_type = thread_type)
+        return url
     
     def add_even(event_name, date, user):
         user.add_event(event_name, date)
@@ -100,19 +130,34 @@ class Bot(Client):
         global got_login
 
         
-        if got_login == 0:
-            self.send(Message(text = "Salut! Nu stiu inca cum te cheama. Care este numele tau?"), thread_id = author_id, thread_type = ThreadType.USER)
-            got_login = 1
+        if not got_login:
+            message = "Salut! Nu stiu inca cum te cheama. Care este numele tau?"
+            self.send(Message(text = message ), thread_id = author_id, thread_type = ThreadType.USER)
+            got_login = True
+        
+        elif got_login:
             
-        elif got_login == 1:
-            
-            got_login = 0
+            got_login = False
             name = message_object.text
             user = User(id = author_id,name = name)
             users.append(user)
-            self.send_all(message = name + ' a intrat pe server! Hai sa-l salutam')
+            message = name + ' a intrat pe server! Hai sa-l salutam'
+            self.send_all(message = message)
+    
+        return message
+            
 
 
+    def logoutUser(self, author_id):
+        user = get_user(author_id)
+        name = user.name
+        users.remove(get_user(author_id))
+        message = name + ' a parasit conversatia'
+        self.send_all(message = message)
+        
+        return message
+        
+        
 
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         global addresses
@@ -124,15 +169,24 @@ class Bot(Client):
                 self.loginUser( author_id, message_object)
                    
             else:
-                cere_vremea = 0
-                cere_calendar = 0
+                cere_vremea = False
+                cere_calendar = False
+                cere_logout = False
+                change_nickname = False
+                
                 if 'botule' in message_object.text.lower() and 'vremea' in message_object.text.lower():
-                    cere_vremea = 1
+                    cere_vremea = True
                 if 'botule' in message_object.text.lower() and 'calendar' in message_object.text.lower():
-                    cere_calendar = 1
+                    cere_calendar = True
                     
+                if 'botule' in message_object.text.lower() and 'logout' in message_object.text.lower():
+                    cere_logout = True
+                    
+                     
+                    
+
                 if 'botule' in message_object.text.lower() and 'eveniment' in message_object.text.lower():
-                    cere_adauga_eveniment = 1
+                    cere_adauga_eveniment = True
                     sir = message_object.text.lower()
                     sir2 = message_object.text.lower()
                     data= sir.split('data=')
@@ -141,12 +195,37 @@ class Bot(Client):
                     data2= data[0] + data[1]
                     nume= nume[1]
                     add_even(nume, data2, get_user(author_id))
+                    
                 
-                self.send_all(message = get_user(author_id).name + " -> " + message_object.text, author_id = author_id)
-                if cere_vremea == 1:
+                
+                # botule schimba nickname bianca new_nick 
+                if 'botule' in message_object.text.lower() and 'nickname' in message_object.text.lower():
+                    change_nickname = True
+                    
+                    substring = message_object.text.split("nickname ")
+
+                    [name, nickname] = substring.split(" ")
+                    target = get_user_by_name(lower(name))
+                    if target != -1:
+                        self.change_nickname(author_id, target, nickname)
+                    else:
+                        self.send(Message(text = "Userul ales nu exista"), thread_id = author_id, thread_type = ThreadType.USER)
+
+
+                
+                if cere_vremea:
                     self.cere_vremea(message_object)
-                if cere_calendar == 1:
+                
+                elif cere_calendar:
                     self.cere_calendar(author_id, thread_type)
+                elif cere_logout:
+                    self.logout(author_id)
+                    
+                    
+                self.send_all(message = get_user(author_id).name + " -> " + message_object.text, author_id = author_id)
+
+                   
+                    
                     
                     
 # email = input("email: ")
@@ -155,3 +234,4 @@ email = 'email'
 password = 'password'
 client = Bot(email, password)
 client.listen()
+
