f9a1ab2a5a4ee51e9f939d213e5766dce41cd5e7
Necula Florin Alexandru <45423257+florinalexandrunecula@users.noreply.github.com>
2020-06-11 17:45:23 +0300

    Update serverCalendar.py

--- serverCalendar.py
+++ serverCalendar.py
@@ -7,16 +7,16 @@ import json
 app = Flask(__name__)
 
 @app.route('/Calendar')
-def calendar(): 
+def calendar_user():
     name = request.args.get('name', None)
     dictionary = request.args.get('dict', None)
-    dictionary = json.loads(dictionary)
+    dicti = json.loads(dictionary)
     currentMonth = datetime.now().month
     currentYear = datetime.now().year
     text_cal = calendar.HTMLCalendar(firstweekday = 0)
     year = currentYear
     month = currentMonth
-    return text_cal.formatmonth(year, month) + "Evenimente: " + dictionary
+    return text_cal.formatmonth(year, month) + "Evenimente: <br/>" + str(dicti)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 443)
